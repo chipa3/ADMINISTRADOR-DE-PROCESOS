98cf5c6dbb12d3b2716bbe2117619d366a74aa6d
chipa3 <chipa_124578@outlook.com>
2019-09-18 20:55:07 -0600

    avances

---
+++ administrador/src/administrador/administrador_de_tareas.java
@@ -0,0 +1,234 @@
+
+package administrador;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class administrador_de_tareas extends javax.swing.JFrame {
+ 
+    DefaultTableModel mt;
+    String[] enc = {"Procesos"};
+    String[][] datos = null;
+    
+    public administrador_de_tareas() {
+        
+        initComponents();
+        this.setLocationRelativeTo(null);
+        procesos.setFocusable(false);
+        mt= new DefaultTableModel(datos,enc);     
+        this.jTable1.setModel(mt); 
+    }
+
+   
+    public static List listRunningProcesses() {
+    List<String> processes = new ArrayList<String>();
+    try {
+        String line;
+    Process p = Runtime.getRuntime().exec("tasklist.exe /nh");
+    BufferedReader input = new BufferedReader
+    (       new InputStreamReader(p.getInputStream()));
+    while ((line = input.readLine()) != null)
+    {
+    if (!line.trim().equals("")) {
+
+    processes.add(line.substring(0, line.indexOf(" ")));
+    }
+    }
+    input.close();
+    }
+        catch (Exception err) {
+        err.printStackTrace();
+}
+        return processes;
+}
+    void LimpiarTabla(){
+        int a =mt.getRowCount()-1;  //Índices van de 0 a n-1
+            for(int i=a;i>=0;i--){  
+            mt.removeRow(i);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        procesos = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jButton1.setText("PROCESOS");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("MATAR PROCESO");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("ADMINISTRADOR DE TAREAS");
+
+        jLabel2.setText("TOTAL DE PROCESOS: ");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(35, 35, 35)
+                        .addComponent(procesos, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(37, 37, 37)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(154, 154, 154)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 713, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel1)
+                .addGap(35, 35, 35)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(procesos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       LimpiarTabla();
+    List<String> processes = listRunningProcesses();
+    String result = "";
+    Iterator<String> it = processes.iterator();
+    int i = 0;
+    this.procesos.setText(String.valueOf(processes.size()));
+    while (it.hasNext()) {
+    result += it.next();
+          i++;
+       if (i==1) {
+        Object[] s={result};
+        mt.addRow(s);
+            result="";
+            i = 0;
+        }
+}
+   
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       String cm=JOptionPane.showInputDialog("Nombre del Proceso:");
+ String osName = System.getProperty("os.name");
+ String cmd="";
+ if(osName.toUpperCase().contains("WIN")){
+	cmd+="taskkill "+cm;
+ }else{
+	cmd+="taskkill "+cm;
+ }
+ Process hijo;
+ try {
+	hijo = Runtime.getRuntime().exec(cmd);
+	hijo.waitFor();
+	if ( hijo.exitValue()==0){
+	 /*JFrame Matando = null;
+         Matando = new Matando();
+         Matando.setVisible(true);*/
+	}else{
+	JOptionPane.showMessageDialog(null,"El gato no pudo matar el proceso!. Excpcion: " + hijo.exitValue()+"n");
+	}
+ } catch (IOException e) {
+	JOptionPane.showMessageDialog(null,"Incapaz de matar Proceso");
+ } catch (InterruptedException e) {
+	JOptionPane.showMessageDialog(null,"Incapaz de matar Proceso");
+    }//GEN-LAST:event_jButton2ActionPerformed
+    }
+  
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(administrador_de_tareas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(administrador_de_tareas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(administrador_de_tareas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(administrador_de_tareas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new administrador_de_tareas().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField procesos;
+    // End of variables declaration//GEN-END:variables
+}
