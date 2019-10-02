40008468ee01f45a559c13ed95d6b79562daef7d
bmazariegos <bryanestuard3@gmail.com>
2019-10-02 17:10:58 -0600

    Administrador de tareas version 2.0

--- administrador/src/administrador/administrador_de_tareas.java
+++ administrador/src/administrador/administrador_de_tareas.java
@@ -4,102 +4,179 @@ package administrador;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import javax.swing.JFrame;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class administrador_de_tareas extends javax.swing.JFrame {
  
-    DefaultTableModel mt;
-    String[] enc = {"Procesos"};
-    String[][] datos = null;
+    private DefaultTableModel modelo;
     
     public administrador_de_tareas() {
         
         initComponents();
         this.setLocationRelativeTo(null);
-        procesos.setFocusable(false);
-        mt= new DefaultTableModel(datos,enc);     
-        this.jTable1.setModel(mt); 
+        No_procesos.setFocusable(false);
+        mostrar_procesos();
     }
 
    
-    public static List listRunningProcesses() {
+    /*   public static List listRunningProcesses() {
     List<String> processes = new ArrayList<String>();
+    List<String> processes2 = new ArrayList<String>();
     try {
-        String line;
+    String line;
     Process p = Runtime.getRuntime().exec("tasklist.exe /nh");
     BufferedReader input = new BufferedReader
     (       new InputStreamReader(p.getInputStream()));
     while ((line = input.readLine()) != null)
     {
     if (!line.trim().equals("")) {
-
+    
     processes.add(line.substring(0, line.indexOf(" ")));
     }
     }
     input.close();
     }
-        catch (Exception err) {
-        err.printStackTrace();
-}
-        return processes;
-}
-    void LimpiarTabla(){
-        int a =mt.getRowCount()-1;  //Índices van de 0 a n-1
-            for(int i=a;i>=0;i--){  
-            mt.removeRow(i);
-        }
+    catch (Exception err) {
+    err.printStackTrace();
     }
+    return processes;
+    }*/
+    private void mostrar_procesos(){
+    int ICol=0,ICont=0;
+        modelo = (DefaultTableModel) jtabla_datos.getModel();
+        Object[] Fila = new  Object[5];
+        int i=0;
+        String StrAuxi = "";
+            
+            
+                try {
+            String line;
+            Process p = Runtime.getRuntime().exec(System.getenv("windir") +"\\system32\\"+"tasklist.exe");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                    System.out.println(line);
+                if(i>=4){
+                    ICont=0;
+                   while(ICont<=4){
+                    String[] sep = line.split("\\s+");
+                    if(ICont!=4){
+                    Fila[ICont] = sep[ICont];
+                    }else{
+                       Fila[ICont] = sep[ICont]+" "+sep[ICont+1]; 
+                    }
+                    ICont++;
+                   }
+                //agregamos el arreglo de la cada fila a la tabla
+                modelo.addRow(Fila);
+                //Asignamos y/o aplicamos el modelo a nuestra tabla
+                jtabla_datos.setModel(modelo);
+                //DefaulTableCellRenderer Alinear = new DefaulTableCellRenderer();
+                
+                }
+        i++;
+            }
+            input.close();
+            No_procesos.setText(String.valueOf(i));
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        
+    }
+    
+
+    void LimpiarTabla(){
+        jtabla_datos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "PID", "Tipo de sesión ", "Número de sesión", "Uso de memoria"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+    }
+    
+    public void terminarP(){
+        modelo = (DefaultTableModel) jtabla_datos.getModel();
+        String StrCelda = String.valueOf(modelo.getValueAt(jtabla_datos.getSelectedRow(), 0));
+        if(StrCelda==""){ 
+          JOptionPane.showMessageDialog(null, "ERROR, No se ha selecionado ningun proceso","Error", JOptionPane.INFORMATION_MESSAGE); 
+        }else{
+        try {
+          Process hijo;
+          hijo = Runtime.getRuntime().exec("taskkill /F /IM "+StrCelda);
+          hijo.waitFor();
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(administrador_de_tareas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+                    
+}
+
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jtabla_datos = new javax.swing.JTable();
+        jIniciar_procesos = new javax.swing.JButton();
+        jterminar_procesos = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        procesos = new javax.swing.JTextField();
+        No_procesos = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Administrador de tareas");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jtabla_datos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {},
-                {},
-                {},
-                {}
+
             },
             new String [] {
-
+                "Nombre", "PID", "Tipo de sesión ", "Número de sesión", "Uso de memoria"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
 
-        jButton1.setText("PROCESOS");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jtabla_datos);
+
+        jIniciar_procesos.setText("PROCESOS");
+        jIniciar_procesos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jIniciar_procesosActionPerformed(evt);
             }
         });
 
-        jButton2.setText("MATAR PROCESO");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jterminar_procesos.setText("MATAR PROCESO");
+        jterminar_procesos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jterminar_procesosActionPerformed(evt);
             }
         });
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("ADMINISTRADOR DE TAREAS");
 
         jLabel2.setText("TOTAL DE PROCESOS: ");
+
+        No_procesos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                No_procesosActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -111,67 +188,69 @@ public class administrador_de_tareas extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(35, 35, 35)
-                        .addComponent(procesos, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(No_procesos, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jIniciar_procesos, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(37, 37, 37)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(154, 154, 154)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 713, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(28, Short.MAX_VALUE))
+                        .addComponent(jterminar_procesos, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(337, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
-                .addComponent(jLabel1)
-                .addGap(35, 35, 35)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 454, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jIniciar_procesos)
+                    .addComponent(jterminar_procesos))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(procesos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(No_procesos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       LimpiarTabla();
-    List<String> processes = listRunningProcesses();
-    String result = "";
-    Iterator<String> it = processes.iterator();
-    int i = 0;
-    this.procesos.setText(String.valueOf(processes.size()));
-    while (it.hasNext()) {
-    result += it.next();
-          i++;
-       if (i==1) {
+    private void jIniciar_procesosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jIniciar_procesosActionPerformed
+        
+        LimpiarTabla();
+        mostrar_procesos();
+        /*
+        List<String> processes = listRunningProcesses();
+        String result = "";
+        String result2 = "";
+        Iterator<String> it = processes.iterator();
+        int i = 0;
+        this.No_procesos.setText(String.valueOf(processes.size()));
+        while (it.hasNext()) {
+        result += it.next();
+        i++;
+        if (i==1) {
         Object[] s={result};
         mt.addRow(s);
-            result="";
-            i = 0;
+        result="";
+        i = 0;
         }
-}
+        }*/
    
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jIniciar_procesosActionPerformed
     // Boton de cerrar o matar procesos
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       String cm=JOptionPane.showInputDialog("Nombre del Proceso:");
+    private void jterminar_procesosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jterminar_procesosActionPerformed
+       
+        terminarP();
+        LimpiarTabla();
+        mostrar_procesos();
+      /* String cm=JOptionPane.showInputDialog("Nombre del Proceso:");
  String osName = System.getProperty("os.name");
  String cmd="";
  if(osName.toUpperCase().contains("WIN")){
      
-	cmd+="TASKKILL /IM "+cm+".exe";
+	cmd+="TASKKILL /F /IM "+cm+".exe"+" /T";
  }else{
-	cmd+="TASKKILL /IM "+cm+".exe";
+	//cmd+="TASKKILL /F /IM "+cm+".exe"+" /T";
  }
  Process hijo;
  try {
@@ -182,8 +261,15 @@ public class administrador_de_tareas extends javax.swing.JFrame {
 	JOptionPane.showMessageDialog(null,"Incapaz de matar Proceso");
  } catch (InterruptedException e) {
 	JOptionPane.showMessageDialog(null,"Incapaz de matar Proceso");
-    }//GEN-LAST:event_jButton2ActionPerformed
-    }
+    }//GEN-LAST:event_jterminar_procesosActionPerformed
+    
+    */}
+    private void No_procesosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_No_procesosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_No_procesosActionPerformed
+
+       
+
   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -218,12 +304,11 @@ public class administrador_de_tareas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField No_procesos;
+    private javax.swing.JButton jIniciar_procesos;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField procesos;
+    private javax.swing.JTable jtabla_datos;
+    private javax.swing.JButton jterminar_procesos;
     // End of variables declaration//GEN-END:variables
 }
